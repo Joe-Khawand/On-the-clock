47f34eff7406f1a66628370c59fa51ac027fcf58
paulwrg <94805384+paulwrg@users.noreply.github.com>
2022-06-06 14:51:54 +0200

    Deleted all compiled files from third party folder

--- library/third_party/src/jpeg/jpge.d
+++
@@ -1,5 +0,0 @@
-../../../library//third_party/src/jpeg/jpge.o: \
-  ../../../library//third_party/src/jpeg/jpge.cpp \
-  ../../../library//third_party/src/jpeg/jpge.h
-
-../../../library//third_party/src/jpeg/jpge.h:
--- library/third_party/src/simplexnoise/simplexnoise1234.d
+++
@@ -1,5 +0,0 @@
-../../../library//third_party/src/simplexnoise/simplexnoise1234.o: \
-  ../../../library//third_party/src/simplexnoise/simplexnoise1234.cpp \
-  ../../../library//third_party/src/simplexnoise/simplexnoise1234.hpp
-
-../../../library//third_party/src/simplexnoise/simplexnoise1234.hpp:
